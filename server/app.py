21e5478c8ae0d9c7628336511260ea508b7a5c78
Hagee <hagee.ktr@outlook.com>
2025-06-24 21:36:22 +0300

    [Fix]: Debug .env

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ api = Api()
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("FLASK_SQLALCHEMY_DATABASE_URI")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
